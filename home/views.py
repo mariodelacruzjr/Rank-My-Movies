a5f103fc0bf45399c1d70233b6407897641b9401
Mario <mariodelacruzjr7@gmail.com>
2023-03-28 18:27:27 -0500

    Brought movie from api to dashboard

--- home/views.py
+++ home/views.py
@@ -11,17 +11,20 @@ def HomeView(request):
     response = requests.get(f'https://api.themoviedb.org/3/trending/all/week?api_key=3372059c7957b772cf7c72b570ae110f')
     trending_all_week_results = json.loads(response.content)['results']
     
-    context = {
-        
-        'trending_all_week_results': trending_all_week_results
-    }
-    return render(request,'home.html',context)
+
+    return render(request,'home.html')
 
 
 
 @login_required
 def DashBoardView(request):
-    return render(request, 'dashboard.html')
+    response = requests.get(f'https://api.themoviedb.org/3/trending/all/week?api_key=3372059c7957b772cf7c72b570ae110f')
+    trending_all_week_results = json.loads(response.content)['results']
+    context = {
+        
+        'trending_all_week_results': trending_all_week_results
+    }
+    return render(request, 'dashboard.html', context)
 
 def register(request):
     if request.method=='POST':
