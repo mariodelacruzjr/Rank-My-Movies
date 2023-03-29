cf2092b84c9604a8b0f6f1182324dacd45dfbcfa
Mario <mariodelacruzjr7@gmail.com>
2023-03-29 15:00:36 -0500

    user can add movie to favorites

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('favorites/', views.Favorites, name='favorites')
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,10 @@ from .utils import tmdb
 from django.contrib.auth.forms import UserCreationForm
 import requests
 import json
+from .models import Movie
+from django.http import HttpResponse
+from django.http import HttpResponseNotAllowed
+
 
 # Create your views here.
 
@@ -36,6 +40,30 @@ def register(request):
         form=UserCreationForm()
 
     return render(request, 'register.html', {'form': form})
+
+def Favorites(request):
+    if request.method == 'POST':
+        # Retrieve movie data from TMDB API based on the movie_id parameter from the POST request
+        api_key = '3372059c7957b772cf7c72b570ae110f'
+        movie_id = request.POST.get('movie_id')
+        endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}'
+        response = requests.get(endpoint, params={'api_key': api_key})
+        movie_data = response.json()
+
+        # Extract the title and poster path from the movie data
+        movie_title = movie_data['title']
+        movie_poster_path = movie_data['poster_path']
+
+        # Create a new Movie instance and save it to the database
+        movie = Movie(title=movie_title, poster_path=movie_poster_path)
+        movie.save()
+
+        # Redirect the user to a success page
+        return render(request, 'favorites.html')
+
+    return HttpResponseNotAllowed(['POST'])
+
+
 
 
 
